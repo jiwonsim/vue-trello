56fed364633a4b94ca3b2c6e74d018210f0f07f4
심지원 <shark.cametotea@gmail.com>
2019-03-14 15:48:28 +0900

    apply vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import router from './router/index' // './router'와 같다. index.js 생략 가능! 
 import App from './App.vue'
+import store from './store'
 
 new Vue({
   el: '#app',
   router,  
+  store, 
   render : h => h(App)
 })
---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as api from '../api'
+
+Vue.use(Vuex)
+
+
+
+const store = new Vuex.Store({
+    state: {
+        isAddBoard: false
+    }, 
+    mutations: {
+        SET_IS_ADD_BOARD (state, toggle) {
+            state.isAddBoard = toggle
+        }
+    }, 
+    actions: {
+        ADD_BOARD(_, {title}) {
+            // api call 
+            return api.board.create(title) // return promise
+        }
+    }
+})
+
+export default store
